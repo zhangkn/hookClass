712745813fcf4783b2d4468a9f7e9e38a41c24c7
张坤楠 <zhangkunnan@qinbaowan.com>
2017-11-02 14:42:34 +0800

    Initial Commit

---
+++ hookClass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hookClass
+//
+//  Created by devzkn on 02/11/2017.
+//  Copyright © 2017 Weiliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hookClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hookClass
+//
+//  Created by devzkn on 02/11/2017.
+//  Copyright © 2017 Weiliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
